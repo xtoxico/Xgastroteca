da2155e6b1f3d111ce89236d5ce1c045fd608ace
Antonio Tirado <xtoxico@gmail.com>
2026-01-17 20:57:38 +0100

    chore: Update API base URL to a production endpoint and remove platform-specific conditional logic.

--- mobile/lib/core/constants/app_config.dart
+++ mobile/lib/core/constants/app_config.dart
@@ -1,14 +1,9 @@
-import 'package:flutter/foundation.dart';
+
 
 class AppConfig {
   // Static constant for base URL
-  // Note: For Android Emulator use 'http://10.0.2.2:8080'
-  // Note: For iOS Simulator use 'http://localhost:8080'
-  // Note: For Web use 'http://localhost:8080' or relative if served from same origin
   
   static String get apiBaseUrl {
-    if (kIsWeb) return 'http://localhost:8080';
-    if (defaultTargetPlatform == TargetPlatform.android) return 'http://10.0.2.2:8080';
-    return 'http://localhost:8080';
+    return 'https://api-xgastroteca.antoniotirado.com';
   }
 }
